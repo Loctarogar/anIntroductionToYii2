5deb7e9dc5f57989bf3d410fd2ec598f44381f38
Loctarogar <igrsl1990@gmail.com>
2019-08-15 22:21:22 +0300

    model to work with database + pagination

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'morilon',
+    'password' => 'vk3897vk',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
